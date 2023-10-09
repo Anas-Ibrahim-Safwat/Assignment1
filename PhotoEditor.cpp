0d90eff0edf68e1258d9740a275006e36f65ac8b
philokaram <146956626+philokaram@users.noreply.github.com>
2023-10-09 17:59:39 +0300

    Update PhotoEditor.cpp

--- PhotoEditor.cpp
+++ PhotoEditor.cpp
@@ -13,6 +13,110 @@
 using namespace std;
 unsigned char image[SIZE][SIZE];
 
+void loadImage();
+void saveImage();
+void blackAndWhite();
+void invertImage();
+void mergeImages();
+void flipImage(int n);
+void darkenAndLighten();
+void rotateImage(int angle);
+
+
+int main()
+{
+     //the name of program
+    cout<<"           ______________________ \n          |                      | \n          | photo Editor program |\n          |______________________| \n\n\n";
+
+    loadImage();
+
+    while (true){
+
+    cout << "Please choose your option from the following: "<< endl;
+    cout << "1- Black & White Filter" << endl;
+    cout << "2- Invert Filter" << endl;
+    cout << "3- Merge Filter" << endl;
+    cout << "4- Flip Image" << endl;
+    cout << "5- Rotate Image" << endl;
+    cout << "6- Darken And Lighten Image" << endl;
+    cout << "a- save image" << endl;
+    cout << "b- Add another filter" << endl;
+    cout << "c- Enter another image" << endl;
+    cout << "0- Exit" << endl;
+    cout << "your choice is : ";
+
+    char filterNum;
+    cin >> filterNum;
+    if (filterNum == '1')
+        {
+            blackAndWhite();
+            cout<<"It's done\n";
+        } 
+
+        else if (filterNum == '2')
+        {
+            invertImage();
+            cout<<"It's done\n";
+        } 
+
+        else if (filterNum == '3')
+        {
+            mergeImages();
+            cout<<"It's done\n";
+        } 
+
+        else if (filterNum == '4')
+        {
+            cout<<"Do you want flip Image:\n 1-horizontally\n2-vertically\n";
+            int n;
+            flipImage(n);
+            cout<<"It's done\n";
+        } 
+
+        else if (filterNum == '5')
+        {
+            int angle = 0;
+
+            cout << "Please select an angle to rotate: ";
+            cout << "\n\n1- 90\n2- 180\n3- 270\n";
+            
+            cin >> angle;
+            rotateImage(angle);
+            cout<<"It's done\n";
+        } 
+
+        else if (filterNum == '6')
+        {
+            darkenAndLighten();
+            cout<<"It's done\n";
+        } 
+         else if (filterNum == 'a')
+        {  
+            saveImage();
+            cout<<"It's done\n";
+        } 
+         else if (filterNum == 'b')
+        {  
+            continue;
+        }
+         else if (filterNum == 'c')
+        {  
+            loadImage();
+        }  
+         else if (filterNum == '0'){
+            return 0;
+         }
+          
+          else {
+            cout<<"wrong number ,choose a number in the mnue.\n";
+           
+        }
+
+        
+    }
+}
+
+
 
 void loadImage(){
     char imageFileName[100];
@@ -33,8 +137,6 @@ void saveImage()
     strcat (imageFileName, ".bmp");
     writeGSBMP(imageFileName, image);
 }
-
-//----------------------------------------------------------------------------
 
 void blackAndWhite()
 {
@@ -62,6 +164,7 @@ void blackAndWhite()
 }
 
 
+
 void invertImage()
 {
     for (int i = 0; i < SIZE; i++)
@@ -74,19 +177,15 @@ void invertImage()
 
 void mergeImages()
 {
-    unsigned char mergeImage[SIZE][SIZE];
+unsigned char mergeImage[SIZE][SIZE];
     char mergeImageFileName[100];
-
     cout << "Enter the image file name you want to merge this image with: " << endl;
     cin >> mergeImageFileName;
-
     strcat (mergeImageFileName, ".bmp");
     readGSBMP(mergeImageFileName, mergeImage);
 
-    for (int i = 0; i < SIZE ; ++i)
-    {
-        for (int j = 0; j < SIZE ; ++j) 
-        {
+    for (int i = 0; i < SIZE ; ++i) {
+        for (int j = 0; j < SIZE ; ++j) {
             image[i][j] = (image[i][j] + mergeImage[i][j]) / 2;
         }
     }
@@ -95,25 +194,18 @@ void mergeImages()
 
 void flipImage(int n) 
 {
-    if (n == 1)
-    {
-        for (int i = 0; i < SIZE; i++) 
-        {
-            for (int j = 0; j < SIZE / 2; j++) 
-            {
+    if (n == 1){  //horizontally 
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE / 2; j++) {
                 int num = image[i][j];
                 image[i][j] = image[i][SIZE - j];
                 image[i][SIZE - j] = num;
             }
         }
     }
-
-    else
-    {
-        for (int i = 0; i < SIZE; i++) 
-        {
-            for (int j = 0; j < SIZE/2 ; j++) 
-            {
+    else{ //vertically 
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE/2 ; j++) {
                 int num = image[j][i];
                 image[j][i] = image[SIZE-j][i];
                 image[SIZE-j][i] = num;
@@ -121,6 +213,38 @@ void flipImage(int n)
         }
     }
 }
+
+
+void darkenAndLighten()
+{
+   cout << "If you want to make the photo lighter enter 1" << endl;
+    cout << "If you want to make the photo darker enter 2" << endl;
+    int type;
+    cin >> type;
+
+    for (int i = 0; i < SIZE ; ++i) {
+        for (int j = 0; j < SIZE ; ++j) {
+            if(type == 1){
+                if((image[i][j] + (image[i][j]) / 2) > 255){
+                    image[i][j] = 255;
+                }
+                else{
+                    image[i][j] += (image[i][j]) / 2;
+                }
+            }
+            else{
+                if(image[i][j] - ((image[i][j]) / 2) < 0){
+                    image[i][j] = 0;
+                }
+                else{
+                    image[i][j] -= (image[i][j]) / 2;
+                }
+            }
+        }
+
+    }
+}
+
 
 void rotateImage(int angle)
 {
@@ -142,95 +266,5 @@ void rotateImage(int angle)
                 for (int j = 0; j < SIZE; j++)
                     image[i][j] = image_r[i][j];
             }
-    }
-}
-
-
-void darkenAndLighten()
-{
-    cout << "1- Lighten\n2- Darken\n" << endl;
-
-    int type;
-    cin >> type;
-
-    for (int i = 0; i < SIZE ; ++i) 
-    {
-        for (int j = 0; j < SIZE ; ++j) 
-        {
-            if(type == 1)
-                image[i][j] += (256 - image[i][j])/2;
-            else
-                image[i][j] -= (image[i][j]/2);
-        }
-
-    }
-}
-
-//----------------------------------------------------------------------
-
-int main()
-{
-    loadImage();
-
-    cout << "Please select a filter to apply or 0 to exit: "<< endl;
-    cout << "1- Black & White Filter" << endl;
-    cout << "2- Invert Filter" << endl;
-    cout << "3- Merge Filter" << endl;
-    cout << "4- Flip Image" << endl;
-    cout << "5- Rotate Image" << endl;
-    cout << "6- Darken And Lighten Image" << endl;
-    cout << "0- Exit" << endl;
-
-    char filterNum;
-
-    while(cin >> filterNum) {
-
-        if (filterNum == '1')
-        {
-            blackAndWhite();
-        } 
-
-        else if (filterNum == '2')
-        {
-            invertImage();
-        } 
-
-        else if (filterNum == '3')
-        {
-            mergeImages();
-        } 
-
-        else if (filterNum == '4')
-        {
-            int n = 0;
-
-            cout << "choose axis: \n";
-            cout << "1- horizontal\n2- vertical\n";
-
-            cin >> n;
-
-            flipImage(n);
-        } 
-
-        else if (filterNum == '5')
-        {
-            int angle = 0;
-
-            cout << "Please select an angle to rotate: ";
-            cout << "\n\n1- 90\n2- 180\n3- 270\n";
-            
-            cin >> angle;
-            rotateImage(angle);
-        } 
-
-        else if (filterNum == '6')
-        {
-            darkenAndLighten();
-        } 
-
-        else
-            return 0;
-
-        saveImage();
     }
 }
